c1537ceba8770d606273e62e5ec3de5f35bee2b0
shama <shamarorostislav@gmail.com>
2023-11-18 13:01:44 +0300

    Photo works

--- app/src/main/java/com/example/definitionsdefectsbuildingstructures/data/model/Label.kt
+++ app/src/main/java/com/example/definitionsdefectsbuildingstructures/data/model/Label.kt
@@ -1,9 +1,8 @@
 package com.example.definitionsdefectsbuildingstructures.data.model
 
-import kotlinx.coroutines.flow.MutableStateFlow
 import java.util.UUID
 
 data class Label (
     val id: String = UUID.randomUUID().toString(),
-    var fileNames: MutableStateFlow<List<String>> = MutableStateFlow(listOf())
+    var fileName: String = ""
 )
--- app/src/main/java/com/example/definitionsdefectsbuildingstructures/ui/components/workWithDrawing/ZoomableImage.kt
+++ app/src/main/java/com/example/definitionsdefectsbuildingstructures/ui/components/workWithDrawing/ZoomableImage.kt
@@ -1,7 +1,13 @@
 package com.example.definitionsdefectsbuildingstructures.ui.components.workWithDrawing
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Environment
+import android.provider.MediaStore
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTapGestures
@@ -26,6 +32,9 @@ import coil.request.ImageRequest
 import coil.size.Size
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberPermissionState
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.UUID
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -38,6 +47,7 @@ fun ZoomableImage(
     val scale = remember { mutableStateOf(1f) }
     val offsetX = remember { mutableStateOf(0f) }
     val offsetY = remember { mutableStateOf(0f) }
+    val context = LocalContext.current
 
     val permissionState = rememberPermissionState(Manifest.permission.CAMERA)
 
@@ -68,6 +78,19 @@ fun ZoomableImage(
             translationY = offsetY.value
         )
 
+    val cameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data = result.data
+            val bitmap = data?.extras?.get("data") as Bitmap
+            val filename = UUID.randomUUID().toString() + ".jpg"
+            val dir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS).toString()
+
+            saveBitmapToFile(bitmap, filename, dir)
+        }
+    }
+
     val modifierImage: Modifier = if (isZoomEnabled) {
         imageModifier.pointerInput(Unit) {
             if (permissionState.hasPermission) {
@@ -76,6 +99,9 @@ fun ZoomableImage(
                     val coefficientHeight = realHeight.toDouble() / size.height.toDouble()
                     val imageX = offset.x * coefficientWidth
                     val imageY = offset.y * coefficientHeight
+                    val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    cameraLauncher.launch(cameraIntent)
+
                 }
             } else permissionState.launchPermissionRequest()
         }
@@ -98,5 +124,26 @@ fun ZoomableImage(
                 .align(Alignment.Center),
             contentDescription = null,
         )
+    }
+}
+
+fun saveBitmapToFile(bitmap: Bitmap, filename: String, dir: String) {
+    val imageFile = "$dir/$filename"
+
+    try {
+        // Создаем OutputStream для записи данных в файл
+        val outputStream = FileOutputStream(imageFile)
+
+        // Сжимаем изображение и записываем в файл в формате JPEG
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+
+        // Закрываем поток после записи
+        outputStream.close()
+
+        // Теперь у вас есть файл с изображением по указанному пути
+        // imageFile.getAbsolutePath() содержит путь к файлу
+    } catch (e: IOException) {
+        e.printStackTrace()
+        // Обработайте исключение, если произошла ошибка при сохранении
     }
 }
